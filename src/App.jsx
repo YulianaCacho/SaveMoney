3980442747c7caecc4a7b8e1de253e680317f076
Yuli Cacho <166524112+YulianaCacho@users.noreply.github.com>
2025-06-10 17:26:59 +0000

    Addandcreate

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Beneficios from './components/Beneficios';
+import Testimonios from './components/Testimonios';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Beneficios />
+      <Testimonios />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
